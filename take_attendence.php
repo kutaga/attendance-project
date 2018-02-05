a7270e85bcdde1256e5a4f3f2aa4197c6cbae945
Ramadhani99 <rsadiki99@gmail.com>
2018-02-05 22:20:15 +0300

    adding table for student record of attendence

--- take_attendence.php
+++ take_attendence.php
@@ -366,6 +366,83 @@
 					<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
 						<h5 class="txt-dark">blank page</h5>
 					</div>
+                     <!-- Basic Table -->
+					<div class="col-sm-12">
+						<div class="panel panel-default card-view">
+							<div class="panel-heading">
+								<div class="pull-left">
+									<h6 class="panel-title txt-dark">Basic Table</h6>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="panel-wrapper collapse in">
+								<div class="panel-body">
+									<p class="text-muted">Add class <code>table</code> in table tag.</p>
+									<div class="table-wrap mt-40">
+										<div class="table-responsive">
+											<table class="table mb-0">
+												<thead>
+												  <tr>
+													<th>#</th>
+													<th>First Name</th>
+													<th>Last Name</th>
+													<th>Username</th>
+													<th>Role</th>
+												  </tr>
+												</thead>
+												<tbody>
+												  <tr>
+													<td>1</td>
+													<td>Jens</td>
+													<td>Brincker</td>
+													<td>Brincker123</td>
+													<td><span class="label label-danger">admin</span> </td>
+												  </tr>
+												  <tr>
+													<td>2</td>
+													<td>Mark</td>
+													<td>Hay</td>
+													<td>Hay123</td>
+													<td><span class="label label-info">member</span> </td>
+												  </tr>
+												  <tr>
+													<td>3</td>
+													<td>Anthony</td>
+													<td>Davie</td>
+													<td>Davie123</td>
+													<td><span class="label label-warning">developer</span> </td>
+												  </tr>
+												  <tr>
+													<td>4</td>
+													<td>David</td>
+													<td>Perry</td>
+													<td>Perry123</td>
+													<td><span class="label label-success">supporter</span> </td>
+												  </tr>
+												  <tr>
+													<td>5</td>
+													<td>Anthony</td>
+													<td>Davie</td>
+													<td>Davie123</td>
+													<td><span class="label label-info">member</span> </td>
+												  </tr>
+												  <tr>
+													<td>6</td>
+													<td>Alan</td>
+													<td>Gilchrist</td>
+													<td>Gilchrist123</td>
+													<td><span class="label label-success">supporter</span> </td>
+												  </tr>
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- /Basic Table -->
+
 					<!-- Breadcrumb -->
 					<div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
 						
