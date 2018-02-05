7da314b5b5488653b9868eab2e8e43ce66096297
arryjoe <aretasfaustine@gmail.com>
2018-02-05 22:13:11 +0300

    further modification

--- login.php
+++ login.php
@@ -30,12 +30,12 @@
 		
 		<div class="wrapper pa-0">
 			<header class="sp-header">
-				<div class="sp-logo-wrap pull-left">
+			<!--	<div class="sp-logo-wrap pull-left">
 					<a href="index.html">
 						<img class="brand-img mr-10" src="../img/logo.png" alt="brand"/>
-						<span class="brand-text">Bunny</span>
+						<span class="brand-text">Attendency System</span>
 					</a>
-				</div>
+				</div> -->
 				<div class="form-group mb-0 pull-right">
 					<span class="inline-block pr-10">Don't have an account?</span>
 					<a class="inline-block btn btn-warning  btn-rounded btn-outline" href="signup.html">Sign Up</a>
@@ -53,7 +53,7 @@
 								<div class="row">
 									<div class="col-sm-12 col-xs-12">
 										<div class="mb-30">
-											<h3 class="text-center txt-dark mb-10">Sign in to Bunny</h3>
+											<h3 class="text-center txt-dark mb-10">Sign in to Attendency System</h3>
 											<h6 class="text-center nonecase-font txt-grey">Enter your details below</h6>
 										</div>	
 										<div class="form-wrap">
@@ -66,7 +66,7 @@
 													<label class="pull-left control-label mb-10" for="exampleInputpwd_2">Password</label>
 													<a class="capitalize-font txt-primary block mb-10 pull-right font-12" href="forgot-password.html">forgot password ?</a>
 													<div class="clearfix"></div>
-													<input type="password" class="form-control" required="" id="exampleInputpwd_2" placeholder="Enter pwd">
+													<input type="password" class="form-control" required="" id="exampleInputpwd_2" placeholder="Enter password">
 												</div>
 												
 												<div class="form-group">
