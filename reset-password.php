f647d37804af75e64e6774ae02e4d38618c3dbaa
arryjoe <aretasfaustine@gmail.com>
2018-02-06 00:20:48 +0300

    resert password

--- reset-password.php
+++ reset-password.php
@@ -5,7 +5,7 @@
 <head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-		<title>Bunny I Fast build Admin dashboard for any platform</title>
+		<title>Password Reset Page</title>
 		<meta name="description" content="Bunny is a Dashboard & Admin Site Responsive Template by hencework." />
 		<meta name="keywords" content="admin, admin dashboard, admin template, cms, crm, Bunny Admin, Bunnyadmin, premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
 		<meta name="author" content="hencework"/>
@@ -15,10 +15,10 @@
 		<link rel="icon" href="favicon.ico" type="image/x-icon">
 		
 		<!-- vector map CSS -->
-		<link href="../vendors/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css" rel="stylesheet" type="text/css"/>
+		<link href="vendors/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css" rel="stylesheet" type="text/css"/>
 		
 		<!-- Custom CSS -->
-		<link href="dist/css/style.css" rel="stylesheet" type="text/css">
+		<link href="template/dist/css/style.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		<!--Preloader-->
@@ -37,15 +37,15 @@
 							<div class="auth-form  ml-auto mr-auto no-float">
 								<div class="row">
 									<div class="col-sm-12 col-xs-12">
-										<div class="sp-logo-wrap text-center pa-0 mb-30">
+									<!--	<div class="sp-logo-wrap text-center pa-0 mb-30">
 											<a href="index.html">
 												<img class="brand-img mr-10" src="../img/logo.png" alt="brand"/>
 												<span class="brand-text">Bunny</span>
 											</a>
-										</div>
+										</div> -->
 										<div class="mb-30">
 											<h3 class="text-center txt-dark mb-10">Need help with your password?</h3>
-											<h6 class="text-center txt-grey nonecase-font">Enter the email you use for Bunny, and we’ll help you create a new password.</h6>
+											<h6 class="text-center txt-grey nonecase-font">Enter the email you use for Attendency System, and we’ll help you create a new password.</h6>
 										</div>	
 										<div class="form-wrap">
 											<form action="#">
@@ -75,17 +75,17 @@
 		<!-- JavaScript -->
 		
 		<!-- jQuery -->
-		<script src="../vendors/bower_components/jquery/dist/jquery.min.js"></script>
+		<script src="vendors/bower_components/jquery/dist/jquery.min.js"></script>
 		
 		<!-- Bootstrap Core JavaScript -->
-		<script src="../vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-		<script src="../vendors/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js"></script>
+		<script src="vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="vendors/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js"></script>
 		
 		<!-- Slimscroll JavaScript -->
-		<script src="dist/js/jquery.slimscroll.js"></script>
+		<script src="template/dist/js/jquery.slimscroll.js"></script>
 		
 		<!-- Init JavaScript -->
-		<script src="dist/js/init.js"></script>
+		<script src="template/dist/js/init.js"></script>
 	</body>
 
 <!-- Mirrored from hencework.com/theme/bunny/full-width-light/forgot-password.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 29 Jan 2018 03:13:32 GMT -->
