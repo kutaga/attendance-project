d6496d7be3bb373cacb53af52b8271fde97297e1
Daniel Kindimba <daniellykindimba@gmail.com>
2018-02-06 10:33:53 +0300

    exporting footer out of the pages, and including it to each of the page as an entity

--- courses.php
+++ courses.php
@@ -1331,13 +1331,9 @@ Third Year, Bachelor of Computer Science
 
 
 				<!-- Footer -->
-				<footer class="footer container-fluid pl-30 pr-30">
-					<div class="row">
-						<div class="col-sm-12">
-							<p>2017 &copy; Bunny. Pampered by Hencework</p>
-						</div>
-					</div>
-				</footer>
+			  <?php
+        include "footer.php";
+        ?>
 				<!-- /Footer -->
 			</div>
 		</div>
