f72fad8697bffed9454c80267c5158d5cef8e8ad
arryjoe <aretasfaustine@gmail.com>
2018-02-06 15:13:24 +0300

    modification of login & reset-password

--- login.php
+++ login.php
@@ -5,8 +5,8 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 		<title>Attendance Login</title>
-		<meta name="description" content="Bunny is a Dashboard & Admin Site Responsive Template by hencework." />
-		<meta name="keywords" content="admin, admin dashboard, admin template, cms, crm, Bunny Admin, Bunnyadmin, premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
+		<meta name="description" content="Attendance system for secondary school students." />
+		<meta name="keywords" content="admin, admin dashboard, admin template, school, Attendance premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
 		<meta name="author" content="hencework"/>
 		
 		<!-- Favicon -->
--- reset-password.php
+++ reset-password.php
@@ -6,8 +6,8 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 		<title>Password Reset Page</title>
-		<meta name="description" content="Bunny is a Dashboard & Admin Site Responsive Template by hencework." />
-		<meta name="keywords" content="admin, admin dashboard, admin template, cms, crm, Bunny Admin, Bunnyadmin, premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
+		<meta name="description" content="Attendance system for secondary school students." />
+		<meta name="keywords" content="admin, admin dashboard, admin template, cms, Attendance, School, premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
 		<meta name="author" content="hencework"/>
 		
 		<!-- Favicon -->
