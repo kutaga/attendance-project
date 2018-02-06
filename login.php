a894b3bc01b0252aa0b9c95945c29615c5f8acf3
Daniel Kindimba <daniellykindimba@gmail.com>
2018-02-06 15:42:38 +0300

    login page navigation menu inclusion

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-	
+
 <head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
@@ -8,16 +8,16 @@
 		<meta name="description" content="Attendance system for secondary school students." />
 		<meta name="keywords" content="admin, admin dashboard, admin template, school, Attendance premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
 		<meta name="author" content="hencework"/>
-		
+
 		<!-- Favicon -->
 		<link rel="shortcut icon" href="favicon.ico">
 		<link rel="icon" href="favicon.ico" type="image/x-icon">
-		
+
 		<!-- vector map CSS -->
 		<link href="vendors/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css" rel="stylesheet" type="text/css"/>
-		
-		
-		
+
+
+
 		<!-- Custom CSS -->
 		<link href="template/dist/css/style.css" rel="stylesheet" type="text/css">
 	</head>
@@ -27,22 +27,14 @@
 			<div class="la-anim-1"></div>
 		</div>
 		<!--/Preloader-->
-		
+
 		<div class="wrapper pa-0">
-			<header class="sp-header">
-			<!--	<div class="sp-logo-wrap pull-left">
-					<a href="index.html">
-						<img class="brand-img mr-10" src="../img/logo.png" alt="brand"/>
-						<span class="brand-text">Attendency System</span>
-					</a>
-				</div> -->
-				<div class="form-group mb-0 pull-right">
-					<span class="inline-block pr-10">Don't have an account?</span>
-					<a class="inline-block btn btn-warning  btn-rounded btn-outline" href="signup.html">Sign Up</a>
-				</div>
-				<div class="clearfix"></div>
-			</header>
-			
+			<!-- Top Menu Items -->
+			<?php
+	    include "navbar.php";
+	    ?>
+			<!-- /Top Menu Items -->
+
 			<!-- Main Content -->
 			<div class="page-wrapper pa-0 ma-0 auth-page">
 				<div class="container-fluid">
@@ -55,7 +47,7 @@
 										<div class="mb-30">
 											<h3 class="text-center txt-dark mb-10">Sign in to Attendency System</h3>
 											<h6 class="text-center nonecase-font txt-grey">Enter your details below</h6>
-										</div>	
+										</div>
 										<div class="form-wrap">
 											<form action="#">
 												<div class="form-group">
@@ -68,7 +60,7 @@
 													<div class="clearfix"></div>
 													<input type="password" class="form-control" required="" id="exampleInputpwd_2" placeholder="Enter password">
 												</div>
-												
+
 												<div class="form-group">
 													<div class="checkbox checkbox-primary pr-10 pull-left">
 														<input id="checkbox_2" required="" type="checkbox">
@@ -81,32 +73,32 @@
 												</div>
 											</form>
 										</div>
-									</div>	
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-					<!-- /Row -->	
+					<!-- /Row -->
 				</div>
-				
+
 			</div>
 			<!-- /Main Content -->
-		
+
 		</div>
 		<!-- /#wrapper -->
-		
+
 		<!-- JavaScript -->
-		
+
 		<!-- jQuery -->
 		<script src="vendors/bower_components/jquery/dist/jquery.min.js"></script>
-		
+
 		<!-- Bootstrap Core JavaScript -->
 		<script src="vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 		<script src="vendors/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js"></script>
-		
+
 		<!-- Slimscroll JavaScript -->
 		<script src="template/dist/js/jquery.slimscroll.js"></script>
-		
+
 		<!-- Init JavaScript -->
 		<script src="template/dist/js/init.js"></script>
 	</body>
