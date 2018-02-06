d020b0935edce63cbe91c669393d9fcbbec00793
kutaga <domina1504@gmail.com>
2018-02-06 13:30:48 +0300

    modification on class reg form

--- register-class.php
+++ register-class.php
@@ -5,7 +5,7 @@
 <head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-		<title>Bunny I Fast build Admin dashboard for any platform</title>
+		<title>Register Class</title>
 		<meta name="description" content="Bunny is a Dashboard & Admin Site Responsive Template by hencework." />
 		<meta name="keywords" content="admin, admin dashboard, admin template, cms, crm, Bunny Admin, Bunnyadmin, premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
 		<meta name="author" content="hencework"/>
@@ -15,12 +15,12 @@
 		<link rel="icon" href="favicon.ico" type="image/x-icon">
 		
 		<!-- vector map CSS -->
-		<link href="../vendors/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css" rel="stylesheet" type="text/css"/>
+		<link href="vendors/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css" rel="stylesheet" type="text/css"/>
 		
 		
 		
 		<!-- Custom CSS -->
-		<link href="dist/css/style.css" rel="stylesheet" type="text/css">
+		<link href="template/dist/css/style.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		<!--Preloader-->
@@ -31,17 +31,17 @@
 		
 		<div class="wrapper pa-0">
 			<header class="sp-header">
-				<div class="sp-logo-wrap pull-left">
+			<!--	<div class="sp-logo-wrap pull-left">
 					<a href="index.html">
 						<img class="brand-img mr-10" src="../img/logo.png" alt="brand"/>
 						<span class="brand-text">Bunny</span>
-					</a>
-				</div>
+					</a> 
+				</div>  
 				<div class="form-group mb-0 pull-right">
 					<span class="inline-block pr-10">Already have an account?</span>
 					<a class="inline-block btn btn-warning btn-rounded btn-outline" href="login.html">Sign In</a>
 				</div>
-				<div class="clearfix"></div>
+				<div class="clearfix"></div>  -->
 			</header>
 			
 			<!-- Main Content -->
@@ -54,36 +54,30 @@
 								<div class="row">
 									<div class="col-sm-12 col-xs-12">
 										<div class="mb-30">
-											<h3 class="text-center txt-dark mb-10">Sign up to Bunny</h3>
-											<h6 class="text-center nonecase-font txt-grey">Enter your details below</h6>
+											<h3 class="text-center txt-dark mb-10">Class Registration Form</h3>
+											<h6 class="text-center nonecase-font txt-grey">Enter your class details below</h6>
 										</div>	
 										<div class="form-wrap">
 											<form action="#">
 												<div class="form-group">
-													<label class="control-label mb-10" for="exampleInputName_1">Username</label>
-													<input type="email" class="form-control" required="" id="exampleInputName_1" placeholder="Username">
+													<label class="control-label mb-10" for="exampleInputName_1">Name of the class</label>
+													<input type="email" class="form-control" required="" id="exampleInputName_1" placeholder="Class name">
 												</div>
 												<div class="form-group">
-													<label class="control-label mb-10" for="exampleInputEmail_2">Email address</label>
-													<input type="email" class="form-control" required="" id="exampleInputEmail_2" placeholder="Enter email">
+													<label class="control-label mb-10" for="exampleInputEmail_2">Class Teacher</label>
+													<input type="email" class="form-control" required="" id="exampleInputEmail_2" placeholder="teacher name ">
 												</div>
 												<div class="form-group">
-													<label class="pull-left control-label mb-10" for="exampleInputpwd_2">Password</label>
-													<input type="password" class="form-control" required="" id="exampleInputpwd_2" placeholder="Enter pwd">
+													<label class="pull-left control-label mb-10" for="exampleInputpwd_2">Number of student</label>
+													<input type="password" class="form-control" required="" id="exampleInputpwd_2" placeholder="student number">
 												</div>
-												<div class="form-group">
-													<label class="pull-left control-label mb-10" for="exampleInputpwd_3">Confirm Password</label>
-													<input type="password" class="form-control" required="" id="exampleInputpwd_3" placeholder="Enter pwd">
-												</div>
-												<div class="form-group">
-													<div class="checkbox checkbox-primary pr-10 pull-left">
-														<input id="checkbox_2" required="" type="checkbox">
-														<label for="checkbox_2"> I agree to all <span class="txt-primary">Terms</span></label>
+												
+												
 													</div>
 													<div class="clearfix"></div>
 												</div>
 												<div class="form-group text-center">
-													<button type="submit" class="btn btn-warning btn-rounded">sign Up</button>
+													<button type="submit" class="btn btn-warning btn-rounded">register class</button>
 												</div>
 											</form>
 										</div>
@@ -104,17 +98,17 @@
 		<!-- JavaScript -->
 		
 		<!-- jQuery -->
-		<script src="../vendors/bower_components/jquery/dist/jquery.min.js"></script>
+		<script src="vendors/bower_components/jquery/dist/jquery.min.js"></script>
 		
 		<!-- Bootstrap Core JavaScript -->
-		<script src="../vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-		<script src="../vendors/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js"></script>
+		<script src="vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="vendors/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js"></script>
 		
 		<!-- Slimscroll JavaScript -->
-		<script src="dist/js/jquery.slimscroll.js"></script>
+		<script src="template/dist/js/jquery.slimscroll.js"></script>
 		
 		<!-- Init JavaScript -->
-		<script src="dist/js/init.js"></script>
+		<script src="template/dist/js/init.js"></script>
 	</body>
 
 <!-- Mirrored from hencework.com/theme/bunny/full-width-light/signup.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 29 Jan 2018 03:13:29 GMT -->
